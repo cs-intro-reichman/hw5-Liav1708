05a1142da1043d4dab64fd9a8984708a9e52d3a0
Liav Sarfati <liavsarfati@h-MacBook-Pro-sl-Liav.local>
2024-12-08 22:07:39 +0200

    check

--- MyString.java
+++ MyString.java
@@ -10,7 +10,7 @@ public class MyString {
         //System.out.println(subsetOf("hel", "h"));
         //System.out.println(spacedString(hello));
         //System.out.println(randomStringOfLetters(10));
-        System.out.println(remove("abc","abc"));
+        System.out.println(remove("committee","meet"));
         //System.out.println(spacedString(hello));
         //// Put your other tests here.
     }
@@ -130,17 +130,17 @@ public class MyString {
         for(int i = 0;i < arr2.length;i++){
             arr2[i] = str2.charAt(i);
         }
-        for(int i = 0;i < arr1.length;i++){
-            for(int j = 0;j < arr2.length;j++){
-                if(arr1[i] == arr2[j]){
-                    arr1[i] = ' ';
-                    arr2[j] = ' ';
+        for(int i = 0;i < arr2.length;i++){
+            for(int j = 0;j < arr1.length;j++){
+                if(arr2[i] == arr1[j]){
+                    arr2[i] = ' ';
+                    arr1[j] = ' ';
                 }
             }
         }
-        for(int i = 0;i < arr2.length;i++){
-            if(arr2[i] != ' '){
-                newStr += arr2[i];
+        for(int i = 0;i < arr1.length;i++){
+            if(arr1[i] != ' '){
+                newStr += arr1[i];
             }
         }
 
